b89e4c1556a8d624483c810040e57baea8735748
mnbd <mnbdpro@gmail.com>
2023-03-23 09:35:08 +0100

    feat: cartcontext example

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CartContextProvider from './context/CartContext'
 
 
 const router = createBrowserRouter([
@@ -18,6 +19,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CartContextProvider>
     <RouterProvider router={router} />
+    </CartContextProvider>
   </React.StrictMode>,
 )
